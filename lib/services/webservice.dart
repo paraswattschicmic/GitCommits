d954906c68eb9aac960429cfd1c70cc52fedab89
Paras <paras.watts@chicmic.co.in>
2020-01-04 17:44:10 +0530

    Adding fresh controller

--- lib/services/webservice.dart
+++ lib/services/webservice.dart
@@ -1,14 +1,21 @@
 import 'dart:async';
 import 'package:http/http.dart' as http;
 import 'package:GitCommits/utils/constants.dart';
-import 'dart:developer' as developer;
 
 const apiUrl = Constants.COMMITS_URL;
 
 class API {
-  static Future getCommits() {
+  static Future getCommits() async {
     var url = apiUrl;
     print(apiUrl + "apiUrl");
-    return http.get(url);
+    Map<String, String> requestHeaders = {
+      'Content-type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': 'token dd559a7ff149fccf64dcb24d5f587c28636c53f5'
+    };
+    return await http.get(
+      url,
+      headers: requestHeaders,
+    );
   }
 }
--- lib/widgets/commitsList.dart
+++ lib/widgets/commitsList.dart
@@ -3,9 +3,11 @@ import 'package:flutter/widgets.dart';
 import 'package:GitCommits/modals/commitsModal.dart';
 import 'package:GitCommits/services/webservice.dart';
 import 'dart:convert';
-import 'dart:developer' as developer;
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 class CommitListState extends State<CommitsList> {
+  final RefreshController _refreshController = RefreshController();
+
   List<Commits> _commits = List<Commits>();
 
   @override
@@ -15,10 +17,15 @@ class CommitListState extends State<CommitsList> {
   }
 
   _getCommits() {
+    print("inside get commits");
+    setState(() {
+      _commits.clear();
+    });
     API.getCommits().then((response) {
       setState(() {
         Iterable list = json.decode(response.body);
         _commits = list.map((model) => Commits.fromJson(model)).toList();
+        _refreshController.refreshCompleted();
         print('_commits');
         print(_commits.length);
       });
@@ -29,24 +36,33 @@ class CommitListState extends State<CommitsList> {
     super.dispose();
   }
 
-  ListTile _buildItemsForListView(BuildContext context, int index) {
-    return ListTile(
-        title:
-            Text('Name: ' + _commits[index].commit.committer.name ?? 'Name: '));
+  Widget dataColumn(BuildContext context, int index) {
+    return Expanded(
+        child: Column(
+      // align the text to the left instead of centered
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(_commits[index].commit.committer.name),
+        Text(_commits[index].commit.committer.email),
+        Text(_commits[index].commit.committer.date),
+        Text(_commits[index].commit.message),
+      ],
+    ));
   }
 
-  Widget column = Expanded(
+  static Text makeText(String name) {
+    return Text(name, style: TextStyle(fontWeight: FontWeight.bold));
+  }
+
+  Widget headingColumn = Expanded(
     child: Column(
       // align the text to the left instead of centered
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
-        Text(
-          'Name',
-          style: TextStyle(fontSize: 16),
-        ),
-        Text('Email'),
-        Text('Commit Time'),
-        Text('Message'),
+        makeText('Name'),
+        makeText('Email'),
+        makeText('Commit Time'),
+        makeText('Message'),
       ],
     ),
   );
@@ -54,16 +70,29 @@ class CommitListState extends State<CommitsList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Git Commits History Flutter Demo'),
-        ),
-        body: ListView.separated(
-          itemCount: _commits.length,
-          separatorBuilder: (BuildContext context, int index) => Divider(),
-          itemBuilder: (BuildContext context, int index) {
-            return _buildItemsForListView(context, index);
-          },
-        ));
+      appBar: AppBar(title: Text('Git Commits History Flutter Demo')),
+      body: SmartRefresher(
+          controller: _refreshController,
+          enablePullDown: true,
+          onRefresh: _getCommits,
+          child: ListView.separated(
+            itemCount: _commits.length,
+            separatorBuilder: (BuildContext context, int index) => Divider(),
+            padding: const EdgeInsets.all(8.0),
+            itemBuilder: (BuildContext context, int index) {
+              return Row(
+                children: <Widget>[headingColumn, dataColumn(context, index)],
+              );
+
+              // return _buildItemsForListView(context, index);
+            },
+          )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _getCommits,
+        tooltip: 'Refresh',
+        child: Icon(Icons.refresh),
+      ),
+    );
   }
 }
 
